5a0939235788c71e97855e0808a86ac39c7d58da
weibasai <boorga@msn.com>
2019-07-31 08:59:03 +0800

    add home page

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+name = 'xiaopipi'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
 def index():
-	return '<h1>welcome to the home of xiaopipi!</h1><img src="static/images/totoro.gif">'
+	return render_template('index.html', name=name, movies=movies)
+	#return '<h1>welcome to the home of xiaopipi!</h1><img src="static/images/totoro.gif">'
 
 @app.route('/user/<name>')
 def hello(name):
