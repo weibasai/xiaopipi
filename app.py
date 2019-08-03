471cc5dfc8b9ab3ba02161ce81bae946a4070ac4
weibasai <boorga@msn.com>
2019-08-03 11:38:36 +0800

    add database to the watchlist

--- app.py
+++ app.py
@@ -1,28 +1,87 @@
+# -*- coding: utf-8 -*-
+
 
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+import MySQLdb, click
+
 
 app = Flask(__name__)
 
-name = 'xiaopipi'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:deaR_7539509@localhost/todolist'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(20))
+
+class Movie(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(60))
+	year = db.Column(db.String(4))
+
 
 @app.route('/')
 def index():
-	return render_template('index.html', name=name, movies=movies)
+	user = User.query.first()
+	movies = Movie.query.all()
+	return render_template('index.html', user=user, movies=movies)
 	#return '<h1>welcome to the home of xiaopipi!</h1><img src="static/images/totoro.gif">'
 
 @app.route('/user/<name>')
 def hello(name):
 	return '<h2>hi, %s' % name
+
+@app.cli.command()
+def forge():
+	#db.create_all()
+
+	name = 'weibasai'
+	movies = [
+		{'title': 'My Neighbor Totoro', 'year': '1988'},
+	    {'title': 'Dead Poets Society', 'year': '1989'},
+	    {'title': 'A Perfect World', 'year': '1993'},
+	    {'title': 'Leon', 'year': '1994'},
+	    {'title': 'Mahjong', 'year': '1996'},
+	    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+	    {'title': 'King of Comedy', 'year': '1999'},
+	    {'title': 'Devils on the Doorstep', 'year': '1999'},
+	    {'title': 'WALL-E', 'year': '2008'},
+	    {'title': 'The Pork of Music', 'year': '2012'},
+	]
+
+	user = User(name=name)
+	db.session.add(user)
+	for m in movies:
+		movie = Movie(title=m['title'], year=m['year'])
+		db.session.add(movie)
+
+	db.session.commit()
+	click.echo('Done!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
