451517bc6cb708df133a6ee424bd5f3e12863265
weibasai <boorga@msn.com>
2019-08-05 13:38:32 +0800

    add form with add, edit and delete.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask
-from flask import render_template
+
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import MySQLdb, click
 
@@ -10,8 +10,8 @@ import MySQLdb, click
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:deaR_7539509@localhost/todolist'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev' 
 
 db = SQLAlchemy(app)
 
@@ -29,14 +29,56 @@ def inject_user():
 	user = User.query.first()
 	return dict(user=user)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+	if request.method == 'POST':
+		title = request.form.get('title')
+		year  = request.form.get('year')
+
+		if not title or not year or len(year)>4 or len(title)>60:
+			flash('Invalid input!')
+			return redirect(url_for('index'))
+
+		movie = Movie(title=title, year=year)
+		db.session.add(movie)
+		db.session.commit()
+		flash('Item created!')
+		return redirect(url_for('index'))
+
 	user = User.query.first()
 	movies = Movie.query.all()
 	return render_template('index.html', movies=movies)
 	#return '<h1>welcome to the home of xiaopipi!</h1><img src="static/images/totoro.gif">'
 
+#编辑条目
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	if request.method == 'POST':
+		title = request.form.get('title')
+		year  = request.form.get('year')
 
+		if not title or not year or len(year)>4 or len(title)>60:
+			flash('Invalid input!')
+			return redirect(url_for('edit', movie_id=movie_id))
+
+		movie.title = title
+		movie.year  = year
+		db.session.commit()
+		flash('Item updated.')
+		return redirect(url_for('index'))
+	return render_template('edit.html', movie=movie)
+
+#删除条目
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	db.session.delete(movie)
+	db.session.commit()
+	flash('Item deleted.')
+	return redirect(url_for('index'))
+
+	
 @app.cli.command()
 def forge():
 	#db.create_all()
@@ -73,7 +115,7 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     app.run(
-      host='192.168.1.6',
+      host='192.168.1.7',
       port= 5000,
       debug=True
     )
